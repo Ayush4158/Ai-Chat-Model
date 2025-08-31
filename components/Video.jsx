b5ec84628af70d1d7fcfb4f54bfb3ad9fa6c29a5
Ayush4158 <ayushsingh8692@gmail.com>
2025-08-31 19:06:52 +0530

    feat: add object-cover class to video component for improved styling

--- components/Video.jsx
+++ components/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = ({className}) => {
   return (
     <>
-     <video autoPlay loop muted src="/d9db38724325620311f321ae03840527.mp4"  className={`${className}`}/> 
+     <video autoPlay loop muted src="/d9db38724325620311f321ae03840527.mp4"  className={`${className} object-cover`}/> 
     </>
   )
 }
